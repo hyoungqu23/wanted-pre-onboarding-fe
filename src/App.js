f9dbd8051a2b00ea3dcb3209481a8474ddb52c15
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-16 15:52:40 +0900

    🎨 Add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
-import Todo from './components/todo/Todo';
 
 function App() {
   return (
     <div className="App">
       <LoginForm />
-      <SignUpForm />
-      <Todo />
     </div>
   );
 }
