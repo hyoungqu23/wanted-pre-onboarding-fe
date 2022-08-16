4fe43f7b79ab40144a2272b0ee8f054690461ce0
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-16 14:48:02 +0900

    🎨 Add TodoButtons Component

---
+++ src/components/todo/TodoButtons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoButtons = () => {
+  return (
+    <>
+      <button>Complete</button>
+      <button>Edit</button>
+      <button>Delete</button>
+    </>
+  );
+};
+
+export default TodoButtons;
+
+const StyledTodoButtons = styled.div``;
