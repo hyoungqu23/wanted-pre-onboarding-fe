55aec214e7203a4e6babc57bc6500a0958e86aa2
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-16 17:16:04 +0900

    🎨 Add checkEmail, checkPassword Functions

---
+++ src/functions.js
@@ -0,0 +1,9 @@
+export const checkEmail = (email) => {
+  const regExp =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  return regExp.test(email);
+};
+
+export const checkPassword = (password) => {
+  return password.length < 8 ? false : true;
+};
