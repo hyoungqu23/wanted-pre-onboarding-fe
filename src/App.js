7ccc3af7832d9b53427d48a3ba19610386d90801
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-17 08:25:22 +0900

    🎨 Update Router Logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
 import AppLayout from './components/layout/AppLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUpForm from './components/SignUpForm';
+import Todo from './components/todo/Todo';
 
 function App() {
   return (
     <AppLayout>
-      <LoginForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
     </AppLayout>
   );
 }
