812810a156cc23fc0f7f1a181812ad7ece87a3d4
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-17 08:24:02 +0900

    🎨 Add SignUP Logic

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,25 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpForm = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    console.log(e);
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/auth/signup',
+        {
+          email,
+          password,
+        },
+      );
+
+      // localStorage.setItem('token', response.data.access_token);
+
+      console.log('response', response);
+
+      navigate('/');
+    } catch (error) {
+      console.log(error, error.message);
+    }
+  };
+
   return (
-    <>
+    <StyledSignUpForm>
       <h1>Sign UP</h1>
       <form>
-        <label>ID</label>
-        <input type="text" />
-        <label>Name</label>
-        <input type="text" />
+        <label>Email</label>
+        <input type="text" onChange={handleEmailChange} />
         <label>Password</label>
-        <input type="password" />
+        <input type="password" onChange={handlePasswordChange} />
         <label>Password Confirm</label>
         <input type="password" />
-        <input type="button" value="Join Us" />
+        <button type="submit" onClick={handleSubmit}>
+          Join Us
+        </button>
       </form>
-    </>
+    </StyledSignUpForm>
   );
 };
 
 export default SignUpForm;
 
-const StyledSignUpForm = styled.div``;
+const StyledSignUpForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+
+    label {
+      font-size: 0.75em;
+    }
+
+    input,
+    button {
+      margin-bottom: 0.5rem;
+      padding: 0.5rem;
+      border-radius: 8px;
+      height: 2rem;
+      border: none;
+      background-color: #f5f5f5;
+
+      &:focus {
+        background-color: #ffc107;
+      }
+
+      &:hover {
+        border: 1px solid #ffc107;
+      }
+    }
+
+    button {
+      font-size: 1rem;
+      background-color: #ffc107;
+
+      &:hover {
+        border: 1px solid black;
+      }
+    }
+  }
+
+  a {
+    color: red;
+    font-weight: bold;
+    text-decoration: none;
+  }
+`;
