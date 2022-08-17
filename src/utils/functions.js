b1d3b23e9ddea9515370d173e346c7e43be50ce3
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-18 01:07:28 +0900

    🎨 Update Validation Functions

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,14 @@
 export const checkEmail = (email) => {
   const regExp =
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
   return regExp.test(email);
 };
 
 export const checkPassword = (password) => {
   return password.length < 8 ? false : true;
+};
+
+export const checkPasswordConfirm = (password, passwordConfirm) => {
+  return password === passwordConfirm ? true : false;
 };
