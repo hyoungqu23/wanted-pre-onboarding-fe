56b6a84de8247239fa090da2714d855d79ba97c5
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-17 16:30:55 +0900

    🎨 Add Get Todo Logic

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import axios from 'axios';
 
 const Todo = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    const response = await axios.get(
+      'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/todos',
+      {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      },
+    );
+    console.log(response);
+    setTodos(response.data);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  console.log(todos);
   return (
     <>
       <h1>Todo</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todos={todos} />
     </>
   );
 };
