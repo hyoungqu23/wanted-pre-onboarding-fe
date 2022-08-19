be535b9aa1581711e75ef40a35b63c4bfe105ff8
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-08-19 15:49:26 +0900

    🎨 Remove getTodos Logic

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
-import TodoList from './TodoList';
 import TodoForm from './TodoForm';
-import axios from 'axios';
+import TodoItem from './TodoItem';
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]);
-
-  const getTodos = async () => {
-    const response = await axios.get(
-      'https://5co7shqbsf.execute-api.ap-northeast-2.amazonaws.com/production/todos',
-      {
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('token'),
-        },
-      },
-    );
-    console.log(response);
-    setTodos(response.data);
-  };
-
-  useEffect(() => {
-    getTodos();
-  }, []);
-
-  console.log(todos);
   return (
     <StyledTodo>
       <h1>Todo</h1>
       <TodoForm />
-      <TodoList todos={todos} />
+      <TodoItem />
     </StyledTodo>
   );
 };
